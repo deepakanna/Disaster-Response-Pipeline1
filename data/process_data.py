649aaf848634c8b56f7e47fcb0cb87384a5a0801
Deepa <110763030+deepakanna@users.noreply.github.com>
2022-10-31 18:44:01 +0530

    Added if_exists='replace'. updated save_data

--- data/process_data.py
+++ data/process_data.py
@@ -78,7 +78,7 @@ def save_data(df, database_filename):
     Dataframe is saved as SQLite database
     '''
     engine = create_engine('sqlite:///'+database_filename)
-    df.to_sql('DisasterResponse', engine, index=False)
+    df.to_sql('DisasterResponse', engine, index=False,if_exists='replace')
 
 
 
